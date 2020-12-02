14fa18a62ae4d6db61a650477854590311b3d818
irtiza1999 <mdirtiza.hossain1999@gmail.com>
2020-12-02 15:27:49 +0600

    firebase added

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+   apiKey: "AIzaSyDFsgGliBeWHBHStEHE88SKdfTHo_fQmWc",
+   authDomain: "burj-al-arab-ishad.firebaseapp.com",
+   databaseURL: "https://burj-al-arab-ishad.firebaseio.com",
+   projectId: "burj-al-arab-ishad",
+   storageBucket: "burj-al-arab-ishad.appspot.com",
+   messagingSenderId: "1032200380770",
+   appId: "1:1032200380770:web:92367cf352d6daf7af0c3b",
+   measurementId: "G-E2TLXR8MLL"
+ };
+ export default firebaseConfig;
